1e0b2fd5d64472f80d4a71645c7cde91085bdfd6
faraz <mfaraz669@yahoo.com>
2024-04-23 18:38:53 +0530

    Initial commit

---
+++ Data Types and Variables/Lectures/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 4;
+        float myFloat = 0.5F;
+        char myChar = 'c';
+        String myString = "hello";
+        double myDouble = 66664;
+
+        System.out.println(myChar);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myInt);
+        System.out.println(myString);
+
+    }
+}
