921ff8e24d03e5b64e6d8ad88bdf909d3009a4a6
faraz <mfaraz669@yahoo.com>
2024-04-23 18:51:22 +0530

    escape added.

---
+++ Data Types and Variables/Lectures/Escape.java
@@ -0,0 +1,8 @@
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("Hello \bworld");
+        System.out.println("Hello \nIndia");
+        System.out.println("Hello \"Hyderabad\"");
+        System.out.println("Hello \tHyderabad");
+    }
+}
