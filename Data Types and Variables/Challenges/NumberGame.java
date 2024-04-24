79c069738459b96e57a1a2aa91a92aa440a4003a
faraz <mfaraz669@yahoo.com>
2024-04-24 12:14:55 +0530

    escape added.

--- Data Types and Variables/Challenges/NumberGame.java
+++ Data Types and Variables/Challenges/NumberGame.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class NumberGame {
     public static void main(String[] args) {
-        int MyNumber = 10;
-        int YourNumber = 20;
-        System.out.println(MyNumber + YourNumber);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to local calculator");
+        System.out.print("Please enter your first number: ");
+        int FirstNumber = input.nextInt();
+        System.out.print("Please enter your second number: ");
+        int SecondNumber = input.nextInt();
+        int sum = FirstNumber + SecondNumber;
+        System.out.println("Your total number is: " + sum);
     }
 }
